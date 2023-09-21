faf820ac485f74f2ac9ec35ae0809e4a1385992d
peter <peter.kmecl@fe.uni-lj.si>
2023-09-21 11:31:16 +0200

    Dodal primer osnovnega python fajla, print stavek, print format , sleep, import, shebang

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+#! /usr/bin/python2
+# za izvajanje pozeni 
+# chmod +x main.py
+# ./main.py
+import time #dodam modul za nadzor casa v programu
+"""#############################
+####Primer dodajanja modulov####
+# ##############################"""
+#from time import sleep
+#from time import *
+#import time as t
 
-def main():
-    print("moje besedilo")
 
-if __name__ == "__main__":
-    main()
+def main(): #Dodamo funkcijo main za omejitev scope
+    print("moje besedilo") #primer print stavka
+    print "test" #se en primer
+    time.sleep(1.54213) #primer cakanja 1.54213 s
+    print("Moje stevilo: %d" % 5) # primer formatiranja stevil v string - guglaj print formats python
+
+if __name__ == "__main__": #preverimo ce je bil program zagnan kot modul ali program
+    main() #kličemo funkcijo main
